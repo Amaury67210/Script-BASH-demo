badb99110544625fe52ba5b494a6f9e2d175a119
SENSNEBRENNER Amaury <amaury.sensenbrenner@etu.unistra.fr>
2023-11-12 18:34:13 +0100

    deuxième commit

--- Script2.sh
+++ Script2.sh
@@ -15,10 +15,8 @@ if [ ! -f "$database_file" ]; then
     exit 1
 fi
 
-# Exemple de requête SQL pour insérer une nouvelle ligne
 insert_query="INSERT INTO customers (FirstName, LastName, Company, Address, City, State, Country, PostalCode, Phone, Email, SupportRepID, CustomerId) VALUES ('Amaury', 'Sensenbrenner', 'Alsace compagny', '17 Rue de Rosheim', 'Strasbourg', 'CA', 'FR', '67000', '+636814218', 'amaury67210@outlook.fr', 3, 1000);"
 
-# Exécution de la requête d'insertion
 sqlite3 "$database_file" "$insert_query"
 
 echo "------------------------"
@@ -26,17 +24,13 @@ echo "Ligne insérée."
 
 sql_query="SELECT * FROM customers WHERE FirstName = 'Amaury' AND LastName = 'Sensenbrenner';"
 
-# Exécution de la requête SQL avec SQLite
 result=$(sqlite3 "$database_file" "$sql_query")
 
-# Affichage des résultats
 echo "Résultats de la requête :"
 echo "$result"
 
-# Exemple de requête SQL pour supprimer la ligne insérée
 delete_query="DELETE FROM customers WHERE FirstName = 'Amaury' AND LastName = 'SensenbrennerF';"
 
-# Exécution de la requête de suppression
 sqlite3 "$database_file" "$delete_query"
 
 echo "Ligne supprimée."
@@ -48,42 +42,34 @@ echo "------------------------"
 
 sql_query="SELECT * FROM customers WHERE CustomerId = 1;"
 
-# Exécution de la requête SQL avec SQLite
 result=$(sqlite3 "$database_file" "$sql_query")
 
-# Affichage des résultats
 echo "Résultats de la requête :"
 echo "$result"
 
 update_query="UPDATE customers SET FirstName = 'Matthéo' WHERE CustomerId = 1;"
 
-# Exécution de la requête SQL avec SQLite
 sqlite3 "$database_file" "$update_query"
 
 echo "Ligne modifiée."
 
 sql_query="SELECT * FROM customers WHERE CustomerId = 1;"
 
-# Exécution de la requête SQL avec SQLite
 result=$(sqlite3 "$database_file" "$sql_query")
 
-# Affichage des résultats
 echo "Résultats de la requête :"
 echo "$result"
 
 update_query="UPDATE customers SET FirstName = 'Luís' WHERE CustomerId = 1;"
 
-# Exécution de la requête SQL avec SQLite
 sqlite3 "$database_file" "$update_query"
 
 echo "Ligne remodifiée"
 
 sql_query="SELECT * FROM customers WHERE CustomerId = 1;"
 
-# Exécution de la requête SQL avec SQLite
 result=$(sqlite3 "$database_file" "$sql_query")
 
-# Affichage des résultats
 echo "Résultats de la requête :"
 echo "$result"
 
@@ -91,7 +77,6 @@ echo "------------------------"
 
 # -----------------------------------------
 
-# Exemple de requête SQL (remplacez ceci par votre propre requête)
 sql_query="SELECT * FROM customers LIMIT 5;"
 
 # Vérifier si la requête SQL est définie
@@ -100,7 +85,6 @@ if [ -z "$sql_query" ]; then
     exit 1
 fi
 
-# Exécution de la requête SQL avec SQLite et formatage des résultats
 sqlite3 -separator ' | ' -header "$database_file" "$sql_query" | while IFS=' | ' read -r col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11 col12 col13 col14 col15; do
     echo "ID: $col1"
     echo "Nom: $col2"
@@ -120,11 +104,10 @@ sqlite3 -separator ' | ' -header "$database_file" "$sql_query" | while IFS=' | '
     echo "------------------------"
 done
 
-# Définition du fichier de sortie pour le traitement
 output_file="result.txt"
 output_file_2="result2.txt"
 
-# Extraction et traitement des données (exemple : calcul de la moyenne des prix des pistes par genre)
+# Requête : Calcul de la moyenne des prix des pistes par genre
 sqlite3 "$database_file" <<EOF > $output_file
 .mode column
 .headers on
@@ -142,13 +125,12 @@ ORDER BY
     AveragePrice DESC;
 EOF
 
-# Affichage du résultat
 cat $output_file
 
 echo "  " >> $output_file
 echo "# Requête : Calcul de la moyenne des prix des pistes par genre" >> $output_file
 
-# Extraction et traitement des données (artistes avec le plus grand nombre de pistes)
+# Requête : Artistes avec le plus grand nombre de pistes
 sqlite3 "$database_file" <<EOF > $output_file_2
 .mode column
 .headers on
@@ -169,12 +151,11 @@ ORDER BY
 LIMIT 5;
 EOF
 
-# Affichage du résultat
 cat $output_file_2
 
 echo "  " >> $output_file_2
 echo "# Requête : Artistes avec le plus grand nombre de pistes" >> $output_file_2
 
-# Nettoyage des fichiers temporaires
+# Remove des fichiers texte
 # rm $output_file
 # rm $output_file_2
